f3236c4cbe41763af31a24023ba56f797b78ddbd
Hassan Mohamed <hassan.mohamed@linkdev.com>
2022-11-02 12:00:39 +0200

    updated app class

--- Lab1Assignment/src/App.java
+++ Lab1Assignment/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+
+    public static void HelloWord() {
         System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloWord();
     }
 }
